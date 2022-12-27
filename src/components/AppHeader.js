c5aceb576ad62c71dfa5ab3ddd0075f9758c0a98
Daniel Lim <daniellim@Daniels-MBP.home>
2022-12-27 14:20:55 -0800

    add tailwindcss with dependencies

---
+++ src/components/AppHeader.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+class AppHeader extends React.Component {
+  render() {
+    return (
+      <header>
+        <div></div>
+      </header>
+    );
+  }
+}
